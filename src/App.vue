95aa095642f0c61014ed1e37d9e135b5d529e582
koffeeya <kavya.beheraj@gmail.com>
2020-12-22 11:07:03 -0500

    added profile pic

--- src/App.vue
+++ src/App.vue
@@ -2,44 +2,59 @@
 <div class='app-wrapper'>
   <div class='title-wrapper'>
     <div class='title-sticky'>
-      <h1 class='title'>KAVYA BEHERAJ</h1>
-      <h3 class='subheading'>Designer and developer creating human-centered data experiences that inspire genuine curiosity.</h3>
-      <br>
-      <br>
-      <h2 class='subheading'>ABOUT ME</h2>
-      <p>
-        I currently serve as a Data Analyst for Community Solutions, a nonprofit ending homelessness. I'm also working on a M.S. in Data Analysis & Visualization at the CUNY Graduate Center.
-        <br>
-        <br>
-        Both roles have given me experience in Tableau, D3, Vue, and Python, but I like using (and learning) any tool that’s right for the job. Check out some of my work and connect with me.
-      </p>
-      <br>
-        <br>
-      <h2 class='subheading'>CONTACT ME</h2>
-      <div class='icon-wrapper'>
-        <div class='contact-icon'>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 12">
-            <path fill-rule="evenodd" d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2zm13 2.383l-4.758 2.855L15 11.114v-5.73zm-.034 6.878L9.271 8.82 8 9.583 6.728 8.82l-5.694 3.44A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.739zM1 11.114l4.758-2.876L1 5.383v5.73z"/>
-          </svg> &nbsp; <a href='mailto:kavya.beheraj@gmail.com' target="_blank" rel="noopener noreferrer">kavya.beheraj@gmail.com</a>
-        </div>
 
-        <div class='contact-icon'>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212h-2.4s.03-6.547 0-7.225h2.4v1.023a5.54 5.54 0 0 0-.016.025h.016v-.025c.32-.493.89-1.193 2.165-1.193 1.58 0 2.764 1.033 2.764 3.252v4.143h-2.4V9.529c0-.972-.348-1.634-1.217-1.634-.664 0-1.059.447-1.233.878-.063.154-.079.37-.079.586v4.035z"/>
-          </svg> &nbsp; <a href='https://www.linkedin.com/in/kavyabeheraj/' target="_blank" rel="noopener noreferrer">kavyabeheraj</a>
+      <div class='maintitle'>
+        <h1 class='title'>KAVYA BEHERAJ</h1>
+        <h3 class='subheading'>Designer and developer creating human-centered data experiences with genuine curiosity.</h3>
+        <br>
+      </div>
+      
+      <div class='aboutme'>
+        <div class='aboutme-header'>
+          <h2 class='subheading'>ABOUT ME</h2>
         </div>
+        <div class='aboutme-text'>
+          <p>
+            I am a full-time data analyst based in Brooklyn, NY working on a M.S. in Data Analysis & Visualization. I like obsessing over details, watching video essays, and going for long walks in the cemetery near my home.
+            <br>
+            <br>
+            I have experience in Tableau, D3, Vue, and Python, but I'm down to use (and learn) any tool that’s right for the job. Check out my work and connect with me! 
+          </p>
+          <br>
+          <br>
+        </div>
+      </div>
 
-        <div class='contact-icon'>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
-          </svg> &nbsp; <a href='https://github.com/koffeeya' target="_blank" rel="noopener noreferrer">koffeeya</a>
+      <div class='contactme'>
+        <h2 class='subheading'>CONTACT ME</h2>
+        <div class='contactme-content'>
+          <div class='contactme-image'>
+            <img class='headshotImg' :src="headshotUrl" alt="Kavya Beheraj" />
+          </div>
+          <div class='icon-wrapper'>
+            <div class='contact-icon'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 15">
+                <path fill-rule="evenodd" d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2zm13 2.383l-4.758 2.855L15 11.114v-5.73zm-.034 6.878L9.271 8.82 8 9.583 6.728 8.82l-5.694 3.44A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.739zM1 11.114l4.758-2.876L1 5.383v5.73z"/>
+              </svg> &nbsp; <a href='mailto:kavya.beheraj@gmail.com' target="_blank" rel="noopener noreferrer">kavya.beheraj@gmail.com</a>
+            </div>
+            <div class='contact-icon'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212h-2.4s.03-6.547 0-7.225h2.4v1.023a5.54 5.54 0 0 0-.016.025h.016v-.025c.32-.493.89-1.193 2.165-1.193 1.58 0 2.764 1.033 2.764 3.252v4.143h-2.4V9.529c0-.972-.348-1.634-1.217-1.634-.664 0-1.059.447-1.233.878-.063.154-.079.37-.079.586v4.035z"/>
+              </svg> &nbsp; <a href='https://www.linkedin.com/in/kavyabeheraj/' target="_blank" rel="noopener noreferrer">kavyabeheraj</a>
+            </div>
+            <div class='contact-icon'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+              </svg> &nbsp; <a href='https://github.com/koffeeya' target="_blank" rel="noopener noreferrer">koffeeya</a>
+            </div>
+          </div>
         </div>
-          
+        
       </div>
 
     </div>
-    
   </div>
+
   <div class='project-wrapper'>
     <ProjectCard v-for="proj in data" :key='proj.id'
     :title='proj.title'
@@ -65,6 +80,7 @@ export default {
   data() {
     return {
       data: null,
+      headshotUrl: "./images/headshot.jpg",
       config: {
         apiKey: process.env.VUE_APP_API_KEY,
         spreadsheetId: process.env.VUE_APP_SPREADSHEET_ID,
@@ -147,6 +163,21 @@ p {
   margin: 0;
 }
 
+.headshotImg {
+  /* border-radius: 1000px; */
+  width: 100%;
+  max-width: 150px;
+  filter: grayscale(100%) contrast(125%);
+  margin: auto;
+}
+
+.contactme-content {
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  column-gap: 5px;
+}
+
+
 .subheading {
   color: var(--pop)
 }
@@ -168,6 +199,8 @@ p {
   top: 35px;
 }
 
+
+
 .contact-icon {
   padding: 10px;
   width: fit-content;
@@ -182,6 +215,10 @@ p {
 .contact-icon>a {
   color: inherit;
   text-decoration: none;
+}
+
+.bi {
+  transform: translateY(2px);
 }
 
 .project-wrapper {
